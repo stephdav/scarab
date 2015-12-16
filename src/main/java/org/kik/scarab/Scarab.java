62917058729a701beb69727edfeb5a83ca896d82
stephdav <stephane.david@soprasteria.com>
2015-12-16 13:49:46 +0100

    initDatabase profile

--- src/main/java/org/kik/scarab/Scarab.java
+++ src/main/java/org/kik/scarab/Scarab.java
@@ -1,5 +1,7 @@
 package org.kik.scarab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,8 +12,19 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Scarab {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Scarab.class, args);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(Scarab.class);
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication app = new SpringApplication(Scarab.class);
+
+        if (args != null && args.length == 1 && "init".equals(args[0])) {
+            LOGGER.info("Add 'initDatabase' profile");
+            app.setAdditionalProfiles("initDatabase");
+        }
+
+        app.run(args);
+
+    }
 
 }
